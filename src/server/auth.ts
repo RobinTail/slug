a7c35ab9ca918e04c3086768b9a9a57718025d35
pheralb <pablohdez4340@gmail.com>
2023-07-09 22:58:36 +0100

    Prepare auth & db config.

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -9,25 +9,12 @@ import DiscordProvider from "next-auth/providers/discord";
 import { env } from "@/env.mjs";
 import { prisma } from "@/server/db";
 
-/**
- * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
- * object and keep type safety.
- *
- * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
- */
 declare module "next-auth" {
   interface Session extends DefaultSession {
     user: {
       id: string;
-      // ...other properties
-      // role: UserRole;
     } & DefaultSession["user"];
   }
-
-  // interface User {
-  //   // ...other properties
-  //   // role: UserRole;
-  // }
 }
 
 /**
